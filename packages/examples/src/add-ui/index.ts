2ec812e9ef096d2dafe709a2a1052cfe15d0bfb5
Nissan Hanina <nissan@yo.ma>
2017-08-27 20:02:18 +0300

    v0.0.0-alpha.0

--- packages/examples/src/add-ui/index.ts
+++ packages/examples/src/add-ui/index.ts
@@ -1,7 +1,7 @@
 
 import { map, combine, switchLatest, mergeArray } from '@most/core'
 import { compose } from '@most/prelude'
-import { domEvent, branch, text, node, component, element, nullSink } from '@fufu/core'
+import { domEvent, branch, text, node, component, element, nullSink } from 'fufu'
 import { pipe, xForver } from '../utils'
 import { newDefaultScheduler } from '@most/scheduler'
 import * as commonSSheet from '../stylesheet'
