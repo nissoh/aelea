5684eaeb3c00658c55477994ddff1fdd3425df98
Nissan Hanina <nissan@yo.ma>
2017-08-27 15:28:14 +0300

    alpha

--- packages/core/src/node.test.ts
+++ packages/core/src/node.test.ts
@@ -1,24 +1,31 @@
-// require('jsdom-global')()
+require('jsdom-global')()
 
-// import { it } from '@typed/test'
-// import { node } from '../index'
-// import { runEffects, tap, take } from '@most/core'
-// import { Stream, Scheduler } from '@most/types'
-// import { newDefaultScheduler } from '@most/scheduler'
+import { it } from '@typed/test'
+import { node } from './index'
+import { runEffects, tap, take } from '@most/core'
+import { Stream, Scheduler } from '@most/types'
+import { newDefaultScheduler } from '@most/scheduler'
+import { curry2 } from '@most/prelude'
+import { pipe } from '../../examples/src/utils'
 
-// const defScheduler = newDefaultScheduler()
+const defScheduler = newDefaultScheduler()
 
-// export function collectEvents <T> (stream: Stream<T>, scheduler: Scheduler) {
-//   const into: any = []
-//   const s = tap(x => into.push({ time: scheduler.now(), value: x }), stream)
-//   return runEffects(s, scheduler).then(() => into)
-// }
+type Event<T> = { time: number, value: T }
 
-// export const otherTest = it('Emits node', ({ ok, equal }) =>
-//   collectEvents(take(1, node), defScheduler).then(x => {
-//     return equal(x[0].value instanceof Node, true)
-//   })
-// )
+export function collectEvents<T> (stream: Stream<T>, scheduler: Scheduler) {
+  const into: Event<T>[] = []
+  const s = tap(x => into.push({ time: scheduler.now(), value: x }), stream)
+  return runEffects(s, scheduler).then(() => into)
+}
+
+const eqInstance = curry2((b: Function, a: Object) => a instanceof b)
+const fstEventValue = <T>(a: Event<T>[]) => a[0].value
+
+const isNode = pipe(fstEventValue, eqInstance(Node))
+
+export const emitNode = it('Emits node', ({ ok }) =>
+  collectEvents(take(1, node), defScheduler).then(pipe(isNode, ok))
+)
 
 
 
