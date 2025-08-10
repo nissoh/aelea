6e8c4962eaa1f262aa90b972d93cb0fee2a6e5ee
Nissan Hanina <1410797+nissoh@users.noreply.github.com>
2025-08-10 18:12:01 +0300

    feat: bump version to 2.4.3 and refactor exports in stream-extended module

--- aelea/src/stream-extended/index.ts
+++ aelea/src/stream-extended/index.ts
@@ -1,4 +1,4 @@
 export * from './buffer.js'
-export { fetchJson } from './fetch.js'
-export { fromWebsocket } from './fromWebsocket.js'
-export { promiseState } from './promise.js'
+export * from './fetch.js'
+export * from './fromWebsocket.js'
+export * from './promise.js'
