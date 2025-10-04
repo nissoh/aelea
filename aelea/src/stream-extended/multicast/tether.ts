4a10138f82202f163d88979f2478e400904bb475
Nissan Hanina <1410797+nissoh@users.noreply.github.com>
2025-10-05 02:51:25 +0300

    feat: update version to 2.5.30 and simplify tether function parameters

--- aelea/src/stream-extended/multicast/tether.ts
+++ aelea/src/stream-extended/multicast/tether.ts
@@ -1,13 +1,4 @@
-import {
-  disposeBoth,
-  disposeNone,
-  disposeWith,
-  type IScheduler,
-  type ISink,
-  type IStream,
-  type ITime,
-  propagateRunEventTask
-} from '../../stream/index.js'
+import { disposeWith, type IScheduler, type ISink, type IStream, type ITime } from '../../stream/index.js'
 import { append, remove } from '../utils.js'
 import { MulticastSink } from './sink.js'
 
@@ -47,8 +38,8 @@ import { MulticastSink } from './sink.js'
  *
  * @returns [primary, tethered] stream tuple
  */
-export const tether = <T>(source: IStream<T>, replayLatest = false): [IStream<T>, IStream<T>] => {
-  const tetherStream = new Tether<T>(replayLatest)
+export const tether = <T>(source: IStream<T>): [IStream<T>, IStream<T>] => {
+  const tetherStream = new Tether<T>()
   return [new PrimaryStream(source, tetherStream), tetherStream]
 }
 
@@ -108,10 +99,6 @@ class Tether<T> extends MulticastSink<T> implements IStream<T> {
   latestValue: T | undefined
   hasValue = false
 
-  constructor(readonly replayLatest: boolean) {
-    super()
-  }
-
   run(sink: ISink<T>, scheduler: IScheduler): Disposable {
     this.sinkList = append(this.sinkList, sink)
 
@@ -120,15 +107,6 @@ class Tether<T> extends MulticastSink<T> implements IStream<T> {
 
       if (i > -1) this.sinkList = remove(this.sinkList, i)
     })
-
-    if (this.replayLatest) {
-      // Emit cached value if available
-      const asapDisposable = this.hasValue
-        ? scheduler.asap(propagateRunEventTask(sink, emitCachedValue, this.latestValue!))
-        : disposeNone
-
-      return disposeBoth(asapDisposable, unsubscribeDisposable)
-    }
 
     return unsubscribeDisposable
   }
